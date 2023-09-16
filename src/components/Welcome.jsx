83111054e7ee451e2ff2eba37ffc6983e98abc13
123Abushad <142307478+123Abushad@users.noreply.github.com>
2023-09-16 21:36:45 +0530

    second commit

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,16 @@
+import { useState, useRef, useEffect } from 'react'
+
+
+function Welcome() {
+ 
+
+  return (
+    <>
+    <div className="flex justify-center h-screen bg-gray-50 dark:bg-gray-900">
+        <h1 className='mt-28 text-white text-3xl'>Welcome</h1>
+   </div>
+    </>
+  )
+}
+
+export default Welcome
